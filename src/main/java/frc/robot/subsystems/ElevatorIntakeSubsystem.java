d01ab263116e78e083b4e66c5cb00f5ffe8877ac
Vyas <trio.sav@gmail.com>
2023-02-16 19:17:24 -0500

    Add UI and Network Tables to switch between CUBE and CONE mode, and invert motor speeds and current limits accordingly

--- src/main/java/frc/robot/subsystems/ElevatorIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorIntakeSubsystem.java
@@ -8,25 +8,38 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ElevatorIntakeSubsystem extends SubsystemBase 
 {
   private CANSparkMax m_NEO550Motor = new CANSparkMax(9, MotorType.kBrushless);
 
+  long currentIntakeMode;
+
   public ElevatorIntakeSubsystem() 
   {
     m_NEO550Motor.setIdleMode(IdleMode.kBrake);
-    m_NEO550Motor.setSmartCurrentLimit(25);
   }
 
   public void setNEOMotorSpeed(double speed)
   {
-    m_NEO550Motor.set(speed);
+    currentIntakeMode = NetworkTableInstance.getDefault().getTable("sidecar695").getEntry("currentIntakeMode").getInteger(-1);
+    if (currentIntakeMode == 1)
+    {
+      m_NEO550Motor.setSmartCurrentLimit(25);
+      m_NEO550Motor.set(speed);
+    }
+    else if(currentIntakeMode == 2)
+    {
+      m_NEO550Motor.setSmartCurrentLimit(25);
+      m_NEO550Motor.set(-0.7*speed);
+    }
   }
 
   @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public void periodic() 
+  {
   }
 }
