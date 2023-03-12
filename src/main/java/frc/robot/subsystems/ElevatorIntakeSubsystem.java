52f3a9d6e0629849f5ac9872d0ab9c9d4a00667c
Vyas <trio.sav@gmail.com>
2023-03-12 16:32:04 -0400

    Fix premature stall

--- src/main/java/frc/robot/subsystems/ElevatorIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorIntakeSubsystem.java
@@ -145,7 +145,7 @@ public class ElevatorIntakeSubsystem extends SubsystemBase
       if (stalltimer.get() > 0.500)
       {
         currentposition = m_encoder.getPosition();
-        if (Math.abs(currentposition - lastposition) < 3)
+        if (Math.abs(currentposition - lastposition) < 2)
         {
           m_intake.setSmartCurrentLimit(3);
           stallhold = true;
